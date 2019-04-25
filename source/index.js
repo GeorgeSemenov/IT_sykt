142aa5c8f6d024cf04a73dae36b91327deca7ef0
Goshanbo <zhora.sem@yandex.ru>
2019-04-25 09:55:30 +0300

    befor retrun menu

--- source/index.js
+++
@@ -1,4 +0,0 @@
-import createMenu from './menu';//Импортируем дефолтную  функцию из menu.js
-
-var menu = createMenu(['Главная','Обомон','Портфелио'],'menu');
-document.body.appendChild(menu);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ const PATHS = {//Объект с двумя свойствами
 };
 
 module.exports = {
-	entry: PATHS.source + '/index.js',
+	entry: {
+		'index': PATHS.source + 'pages/index/index.js',//Обрати внимание для кадой страницы мы создаём свою точку входа которая начинается с .js файла
+		'blog': PATHS.source + 'pages/blog/blog.js'
+	},
 	devServer: {//Можно легко изменить порт, по которому будет находиться сайт и куча других настроек в пункте dev-server
 		contentBase:'./build',//Указываем директорию, откуда будет строиться сайт на локальном сервере(по умолчанию сразу по адрессу localhost:8080 будет выводиться index.html)
 		hot: true //Это указание на то что мы используем горячую замену модулей Hot Module Replacement
@@ -18,9 +21,15 @@ module.exports = {
 	},
 	plugins:[
 		new HtmlWebpackPlugin({// создаём страничку html с нужным тайтлом.
-			//title: 'Webpack app' вместо этой строки в данный плагин мы передадим сразу разметку pug но сразу же скомпилированную (pug-loader'oм)
-			template: PATHS.source + '/index.pug'
-		})
+			filename:'index.html',//Задаём имя генерируемому файлу
+			chunks: ['index'],
+			template: PATHS.source + '/pages/index/index.pug'//в данный плагин мы передадим разметку pug но сразу же скомпилированную (pug-loader'oм)
+		}),
+		new HtmlWebpackPlugin({// создаём страничку html с нужным тайтлом.
+			filename:'blog.html',//Задаём имя генерируемому файлу
+			chunks: ['blog'],
+			template: PATHS.source + '/pages/blog/blog.pug'//в данный плагин мы передадим разметку pug но сразу же скомпилированную (pug-loader'oм)
+		}),
 	],
 	module:{
 		rules: [
