f217706d058a11457ad4e60258893e6a46e2c53d
Goshanbo <zhora.sem@yandex.ru>
2019-05-06 12:15:13 +0300

    Use provide plugin

--- source/pages/blog/blog.js
+++ source/pages/blog/blog.js
@@ -1,7 +1,6 @@
 import createMenu from '../../components/menu/menu';//Импортируем дефолтную  функцию из menu.js
 import './blog.scss';
 import 'normalize.css';
-import 'jquery';
 
 var menu = createMenu(['Главная','Блог'],'menu');
 document.body.appendChild(menu);
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,9 +1,11 @@
 import createMenu from '../../components/menu/menu';//Импортируем дефолтную  функцию из menu.js
 import './index.scss';
 import 'normalize.css';
-import 'jquery';
 
 var menu = createMenu(['Главная','Блог'],'menu');
 document.body.appendChild(menu);
+
+console.log($);
+console.log(jQuery);
 
 console.log('in index.js');
