5cda6c3154c47f0ac2b0b0db4a139c47875ce517
Goshanbo <zhora.sem@yandex.ru>
2019-06-23 20:12:38 +0300

    Перва редакция готового сайта

--- source/components/popup.js
+++ source/components/popup.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-  $(".feedback-button").click(function(){
+
+  //Вызов формы обратной связи
+  $(".call-feedback-form-button").click(function(){
     $("body").css({"overflow":"hidden"});
     $("#feedback-form").fadeIn();
     $("#feedback-form").css({"top":($(window).scrollTop()+"px")});
   })
+  //закрытие формы обратной связи
   $("#form__form-background, .form__close-button-wrapper, .form__send-button").click(function(){
     $("#feedback-form").fadeOut();
+    $("body").css({"overflow":"auto"});
+  })
+
+  //Вызов формы заявки
+  $(".call-request-form-button").click(function(){
+    $("body").css({"overflow":"hidden"});
+    $("#request-form").fadeIn();
+    $("#request-form").css({"top":($(window).scrollTop()+"px")});
+  })
+  //закрытие формы заявки
+  $("#form__form-background, .form__close-button-wrapper, .form__send-button").click(function(){
+    $("#request-form").fadeOut();
     $("body").css({"overflow":"auto"});
   })
 })
